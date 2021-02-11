99d057766a2a48272c65913831aee3d841bcb462
Phil Wang <lucidrains@gmail.com>
2021-02-10 20:11:49 -0800

    scaffold

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'timesformer-pytorch',
+  packages = find_packages(),
+  include_package_data = True,
+  version = '0.0.1',
+  license='MIT',
+  description = 'TimeSformer - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/TimeSformer-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'transformers',
+    'video classification',
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'torch>=1.6'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
