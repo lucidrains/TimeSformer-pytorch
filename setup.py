3f4bf71277595e8061b4fc45df8c4ea572b33fdf
Phil Wang <lucidrains@gmail.com>
2021-07-03 23:47:44 -0700

    0.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'timesformer-pytorch',
   packages = find_packages(),
-  version = '0.3.2',
+  version = '0.3.3',
   license='MIT',
   description = 'TimeSformer - Pytorch',
   author = 'Phil Wang',
